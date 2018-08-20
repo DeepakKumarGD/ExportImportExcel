08e2725e16579d89ee011a35518320a3b958d893
deepak <deepak1992ac@gmail.com>
2018-08-20 22:20:23 +0530

    project structure checkIn

---
+++ src/main/java/com/excel/ImportExportExcel/ImportExportExcelApplication.java
@@ -0,0 +1,12 @@
+package com.excel.ImportExportExcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportExportExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportExportExcelApplication.class, args);
+	}
+}
---
+++ src/test/java/com/excel/ImportExportExcel/ImportExportExcelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.excel.ImportExportExcel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImportExportExcelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
